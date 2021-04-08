9b0367b886f9e851b94fca0260127981a63b079c
DeepanshiVij <55225311+DeepanshiVij@users.noreply.github.com>
2021-04-08 21:23:42 +0530

    Create tdl.php

---
+++ tdl.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<title>TDL Registration </title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<style>
+.button
+{  background-color: #ddd;
+   border: none;
+   color: black;
+   padding: 10px 20px;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   margin: 4px 2px;
+   border-radius: 16px;
+}
+.button:hover
+{  background-color: #f1f1f1;
+}
+a
+{color:black;}
+</style>
+</head>
+<body>
+<br><br><br><br>
+<div class="container">
+<div class="login-box">
+   <div class="row">
+         <div class="col-md-6" style="text-align:center;">
+             <h2><u><b><i>TDL</i></b></u></h2>
+             <br>
+             <h4>I am a : </h4>
+             <form>
+             <div class="form-group">
+                  <button class = button><a href="s_reg.php">Student</button></a>
+              </div>
+              
+              <div class="form-group">
+                  <button class = button><a href="f_log.php">Faculty</button></a>
+              </div>
+              
+              <div class="form-group">
+                  <button class = button><a href="a_log.php">Administrator</button></a>
+             </div>
+            </form>
+         </div>
+      </div>
+</div>
+</div>
+</body>
+</html>
